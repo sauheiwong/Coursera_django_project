90ab6686b047e04a1d3357fa9a018589fc12fc29
sauhei PC <sauheiwong@gmail.com>
2023-04-14 20:53:04 +0800

    create a DefaultRouter for booking

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import *
+
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class MenuSerializer(ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+
+
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    serializer_class = MenuSerializer 
+    model = Menu
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    serializer_class = MenuSerializer 
+    model = Menu
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
